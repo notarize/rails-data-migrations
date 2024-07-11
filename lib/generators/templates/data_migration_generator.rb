6879669bd22ebe4d7760e1441bb63f5a7884547b
Neal Griffith <neal.griffith@notarize.com>
2024-07-11 17:47:29 -0500

    [BIZ-6315] Updates template to include our typical boilerplate

--- lib/generators/templates/data_migration_generator.rb
+++ lib/generators/templates/data_migration_generator.rb
@@ -1,5 +1,12 @@
+# typed: strict
+
 class ClassName < ActiveRecord::DataMigration
+  extend T::Sig
+
+  disable_ddl_transaction!
+
+  sig { void }
   def up
-    # put your code here
+    # put your code to modify data and output results here
   end
 end
